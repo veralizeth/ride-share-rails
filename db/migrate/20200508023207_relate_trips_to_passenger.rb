4242f683a47f8b3ebd7620cd90d662bff4717eb7
Vera <lizeth.veraro@outlook.com>
2020-05-07 19:41:25 -0700

    All the migrations

---
+++ db/migrate/20200508023207_relate_trips_to_passenger.rb
@@ -0,0 +1,5 @@
+class RelateTripsToPassenger < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :trips, :passenger, index: true
+  end
+end
---
+++ db/migrate/20200508023313_relate_trips_to_driver.rb
@@ -0,0 +1,5 @@
+class RelateTripsToDriver < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :trips, :driver, index: true
+  end
+end

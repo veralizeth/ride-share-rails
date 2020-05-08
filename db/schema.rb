cc23ae5a4ea96000715ea237f9cdcdc60fb82b9e
Vera <lizeth.veraro@outlook.com>
2020-05-08 12:42:24 -0700

    Update schema with string DataType for phoneNumber

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_023313) do
+ActiveRecord::Schema.define(version: 2020_05_08_190403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_023313) do
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
-    t.integer "phone_num"
+    t.string "phone_num"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

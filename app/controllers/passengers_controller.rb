37dbd406c902e775c03091d0ac33e6bf7a87ba12
Haben <haben7@gmail.com>
2020-05-08 16:24:05 -0700

    created show action for passengers

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,4 +2,18 @@ class PassengersController < ApplicationController
   def index # Fleching out our index action.
     @passengers = Passenger.all
   end
+
+
+  def show
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
 end

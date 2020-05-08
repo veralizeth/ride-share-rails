e85a78cb96a902afb8f8f32da2673d383d4a41c7
Vera <lizeth.veraro@outlook.com>
2020-05-08 12:41:56 -0700

    routing root to trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: "trips#index"
   resources :trips
   resources :drivers
   resources :passengers
-  
+
 end

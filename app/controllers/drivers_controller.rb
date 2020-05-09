45d23e7678037ca5414fa26d42c6fb12ed15c0a7
Vera <lizeth.veraro@outlook.com>
2020-05-08 18:34:48 -0700

    Drivers show action

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,4 +2,19 @@ class DriversController < ApplicationController
   def index # Fleching out our index action.
     @drivers = Driver.all
   end
+
+  def show
+    # Raise an exception.
+    # Be aware that any string will be converted to 0
+    driver_id = params[:id].to_i
+    # It needs to handle this exception.
+    @driver = Driver.find_by(id: driver_id)
+
+    puts @driver
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
 end

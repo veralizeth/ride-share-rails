d4578e98848793017be9a724abfddcd091c96d79
Haben <haben7@gmail.com>
2020-05-08 19:52:22 -0700

    added update, edit, create, destroy actions

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,5 @@
 class PassengersController < ApplicationController
-  def index # Fleching out our index action.
+  def index # Fleshing out our index action.
     @passengers = Passenger.all
   end
 
@@ -16,4 +16,66 @@ class PassengersController < ApplicationController
   def new
     @passenger = Passenger.new
   end
+
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    
+    if @passenger.save 
+      redirect_to passenger_path(@passenger.id) 
+      return
+    else 
+      render :new 
+    end
+  end
+
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passenger_path(@passenger.id) 
+      return
+    else 
+      render :edit 
+      return
+    end
+  end
+
+
+  def destroy
+    passenger_id = params[:id]
+    passenger = Passenger.find_by(id: passenger_id)
+
+    if passenger.nil?
+      head :not_found
+      return
+    end
+
+  passenger.destroy
+  redirect_to trips_path
+ 
+  end
+
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 end
+
+

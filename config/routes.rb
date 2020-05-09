d52423ef7556a0b2e0174620c0470c8d45401c89
Vera <lizeth.veraro@outlook.com>
2020-05-08 21:04:30 -0700

    root to pages/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "trips#index"
+  root to: "pages#home"
+  
   resources :trips
   resources :drivers
   resources :passengers

47678bfbe9d44f1586fc65b50fb2d9ec88e3ee17
Vera <lizeth.veraro@outlook.com>
2020-05-08 19:52:16 -0700

    update, edit, destroy and create actions

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -17,4 +17,59 @@ class DriversController < ApplicationController
       return
     end
   end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver.id) # go to the index so we can see the driver in the list
+      return
+    else # save failed :(
+      render :edit # show the new driver form view again
+      return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def destroy
+    driver = Driver.find_by(id: params[:id])
+    if driver.nil?
+      head :not_found
+      return
+    else
+      driver.destroy
+      redirect_to drivers_path
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)# Send them to the '/trips' path
+    else
+      render :new, :bad_request
+    end
+  end
+
+  private
+
+  # Method that does params requare, driver sub hash, before submit it.
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
 end

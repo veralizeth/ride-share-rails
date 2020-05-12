b6d69837ec23fab32f1094d484e7208ba6f0d24a
Vera <lizeth.veraro@outlook.com>
2020-05-12 10:19:27 -0700

    deleting available driver function, wrong place

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,14 @@ class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
   has_many :trips, dependent: :nullify
+
+  def available_driver
+    available_driver = Driver.where(available: true)
+    first_driver = available_driver.first
+    if first_driver
+      return first_driver
+    else
+      return nil
+    end
+  end
 end

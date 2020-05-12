b84a35c3796232ae1ee58c37f4538d80b02d43c8
Vera <lizeth.veraro@outlook.com>
2020-05-12 15:25:20 -0700

    total_earnings and avererage rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,6 +5,27 @@ class Driver < ApplicationRecord
 
   has_many :trips,  dependent: :nullify
 
+  def total_earnings
 
+    return self.trips.sum { |trip| (trip.cost - 1.65 ) * 0.8 }
+
+  end
+
+  def average_rating
+    total_rating = 0
+    count = 0
+    self.trips.each do |trip|
+      if trip.rating
+        total_rating += trip.rating
+        count += 1
+      end
+    end 
+
+    if count == 0
+      return 0
+    else
+      total_rating / count 
+    end
+  end
 
 end

2e86f6b35ec5ce4c6b668290d2a3d9ed84085efd
Vera <lizeth.veraro@outlook.com>
2020-05-12 10:19:02 -0700

    funtion to find  the first available driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,14 +5,6 @@ class Driver < ApplicationRecord
 
   has_many :trips,  dependent: :nullify
 
-  def available_driver
-    available_driver = self.drivers.where(available: true)
-    first_driver = available_driver.first
-    if first_driver
-      return first_driver.name
-    else
-      return nil
-    end
-  end
+
 
 end

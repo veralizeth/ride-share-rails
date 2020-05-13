15a6042d7462d667e5bdaed273d7c75dc3500a75
Vera <lizeth.veraro@outlook.com>
2020-05-12 18:50:50 -0700

    New route to trips/rating action

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
-  
+
+  get "/trips/rating", to: "trips#rating", as: "rating"
   resources :trips
+
   resources :drivers
 
   resources :passengers do
     resources :trips, only: [:index, :create]
   end
 
-  get "/trips/:id/rate", to: "trips#rate" , as: "rate"
+ 
+
 end

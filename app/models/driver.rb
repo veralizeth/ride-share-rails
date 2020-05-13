6f144d90bf6e99633ad10fc3adc92f0ab20473dc
Haben <haben7@gmail.com>
2020-05-13 13:06:57 -0700

    add class method to find 1st available driver in driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,6 +5,17 @@ class Driver < ApplicationRecord
 
   has_many :trips,  dependent: :nullify
 
+
+  def self.available_driver
+    available_driver = Driver.where(available: true)
+    first_driver = available_driver.first
+    if first_driver
+      return first_driver
+    else
+      return nil
+    end
+  end
+
   def total_earnings
     return self.trips.sum { |trip| (trip.cost - 165 ) * 0.8 }
   end
